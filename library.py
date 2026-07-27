f3f3434e46eefdac5130fadcf3d220c4aebe6496
AmmaarAgjee <ammaaragjee@gmail.com>
2026-07-27 20:14:10 +0200

    add library totals function

--- library.py
+++ library.py
@@ -1,1 +1,13 @@
-Library Management System
+#Library Management System
+#Ammaar Agjee
+books ={}
+members = {}
+
+next_book_number = 1
+next_member_number = 1
+
+def library_totals():
+    print("Total number of books in the library:", len(books))
+    print("Total number of members in the library:", len(members))
+library_totals()
+    
