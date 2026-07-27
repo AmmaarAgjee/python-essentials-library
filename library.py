971125b64d2754114964f9ec2d6eb8b2d9f67e18
AmmaarAgjee <ammaaragjee@gmail.com>
2026-07-27 20:51:50 +0200

    Add most_borrowed function

--- library.py
+++ library.py
@@ -9,5 +9,8 @@ next_member_number = 1
 def library_totals():
     print("Total number of books in the library:", len(books))
     print("Total number of members in the library:", len(members))
+
+def most_borrowed():
+    print("most borrowed book feature coming soon.")
 library_totals()
-    
+most_borrowed()
